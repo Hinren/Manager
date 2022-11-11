c29427b2b743858a1b5f1ff52388dc206cc3df24
Kamil Karpiński <kamil.k3@outlook.com>
2022-11-11 11:11:51 +0100

    Fresh start with .NET 6

---
+++ ProjectManager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Hinren.ProjectManager
+{
+    public partial class App : Application
+    {
+    }
+}
